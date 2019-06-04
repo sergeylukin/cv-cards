feac1330b92a35be387fb8eaabd5fc2f60dfb2de
Sergey Lukin <contact@sergeylukin.com>
2019-06-04 14:09:51 +0300

    Display experience per every candidate

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,26 @@ import fetch from 'isomorphic-unfetch';
 
 const Index = props => (
   <div>
-    <h1>Candidates</h1>
-    <ul>
-      {props.candidates.map(candidate => (
-        <li key={candidate.id}>
-          <p>{candidate.name}</p>
-        </li>
-      ))}
-    </ul>
+    <h1>Our awesome candidates</h1>
+    {props.candidates.map(candidate => (
+      <div key={candidate.id} className={ "candidate" }>
+        <p>{ `Hello ${candidate.name}` }</p>
+        {candidate.experience.length > 0 ?
+          <ul>
+            {candidate.experience.map(experience => (
+              <li>{ `Worked as: ${experience.JOB_TITLE}, From ${experience.START_DATE} To ${experience.END_DATE}` }</li>
+            ))}
+          </ul>
+          :
+            <ul><li>No work experience :(</li></ul>
+        }
+      </div>
+    ))}
+    <style jsx>{`
+      .candidate {
+        padding: 2rem;
+      }
+    `}</style>
   </div>
 );
 
@@ -17,19 +29,23 @@ Index.getInitialProps = async function() {
   const res = await fetch('http://hs-resume-data.herokuapp.com/');
   const data = await res.json();
 
-  console.log(`Show data fetched. Count: ${data.length}`);
+  console.log(`Data fetched. Count: ${data.length}`);
 
-  let obj = {
+  return {
     candidates: data.map((entry, key) => {
       return {
         id: key,
         name: entry.contact_info.name.formatted_name,
-        experience: "bla bla",
+        experience: entry['experience'].map(experience => {
+          return {
+            JOB_TITLE: experience.title,
+            START_DATE: experience.start_date,
+            END_DATE: experience.end_date,
+          }
+        }),
       }
     })
   };
-  console.log(obj)
-  return obj
 };
 
 export default Index;
